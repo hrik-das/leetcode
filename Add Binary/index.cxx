b2b8b5c45cc172fb024adcd689b523a4db48e8d5
hrik-das <emptynull01@gmail.com>
2024-10-27 23:42:55 +0530

    Add Binary Refactored

--- Add Binary/index.cxx
+++ Add Binary/index.cxx
@@ -1,20 +1,23 @@
 # include<iostream>
 # include<string>
-# include<bitset>
-# include<sstream>
-class Solution{
+class Solution {
     public:
-    std::string addBinary(const std::string& a, const std::string& b){
-        std::bitset<1024> first_number(a);
-        std::bitset<1024> second_number(b);
-        std::bitset<1024> sum = first_number.to_ullong() + second_number.to_ullong();
-        std::string result = sum.to_string();
-        size_t pos = result.find("1");
-        if(pos != std::string::npos) return result.substr(pos);
-        return "0";
+    std::string addBinary(const std::string& a, const std::string& b) {
+        std::string result;
+        int carry = 0, i = a.size() - 1, j = b.size() - 1;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            result = char(sum % 2 + '0') + result;
+            carry = sum / 2;
+        }
+        return result;
     }
 };
-int main(int argc, const char** argv){
+
+int main(int argc, const char **argv) {
     Solution ob;
     std::string a = "1010";
     std::string b = "1011";
